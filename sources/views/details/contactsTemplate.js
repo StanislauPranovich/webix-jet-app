0ef32df2a1d5d98874683a5fa115af5b1a7db6d6
StanislauPranovich <spranovich@xbsoftware.by>
2022-04-26 14:58:20 +0300

    overwritted contactsTemplate

--- sources/views/details/contactsTemplate.js
+++ sources/views/details/contactsTemplate.js
@@ -1,4 +1,4 @@
-import {JetView} from "webix-jet";
+import { JetView } from "webix-jet";
 
 import contactsData from "../../models/contactsData";
 import statusesData from "../../models/statusesData";
@@ -6,48 +6,47 @@ import statusesData from "../../models/statusesData";
 export default class ContactsTemplate extends JetView {
 	config() {
 		return {
-			view: "toolbar",
-			localId: "contactsTemplate",
 			rows: [
 				{
-					cols: [
-						this.createLabel("FirstName", "FirstName", "text-align-end"),
-						this.createLabel("LastName", "LastName"),
-						this.createButton("<span class='fas fa-trash'></span> Delete", "webix_primary", true),
-						this.createButton("<span class='fas fa-pen'></span> Edit", "webix_primary", true)
-					]
-				},
-				{
-					cols: [
+					view: "toolbar",
+					localId: "contactsHeader",
+					rows: [
 						{
-							rows: [
-								{
-									view: "template",
-									name: "Photo",
-									height: 150
-								},
-								this.createLabel("Status", "StatusID", "text-align-center", statusesData, "#Value#")
-							]
-						},
-						{
-							rows: [
-								this.createLabel("Email", "Email", "text-align-center"),
-								this.createLabel("Skype", "Skype", "text-align-center"),
-								this.createLabel("Job", "Job", "text-align-center"),
-								this.createLabel("Company", "Company", "text-align-center")
-							]
-						},
-						{
-							rows: [
-								this.createLabel("Birthday", "Birthday", "text-align-center"),
-								this.createLabel("Address", "Address", "text-align-center")
+							cols: [
+								this.createLabel("FirstName", "FirstName", "text-align-end"),
+								this.createLabel("LastName", "LastName"),
+								this.createButton("<span class='fas fa-trash'></span> Delete", "webix_primary", true),
+								this.createButton("<span class='fas fa-pen'></span> Edit", "webix_primary", true)
 							]
 						}
 					]
 				},
-				{}
+				{
+					localId: "contactsTemplate",
+					template: obj => `
+						<div class="contactsInfo">
+							<div class="contactsInfoFirstColumn">
+								<img class="contactsInfoPhoto" src=${obj.Photo}  />
+								<p>${statusesData.getItem(obj.StatusID)?.value || "No Status"}</p>
+							</div>
+							<div>
+								<p><span class="fas fa-envelope"></span> ${obj.Email || "-"}</p>
+								<p><span class="fab fa-skype"><span/> ${obj.Skype || "-"}</p>
+								<p><span class="fas fa-briefcase"></span> ${obj.Job || "-"}</p>
+								<p><span class="fas fa-building"></span> ${obj.Company || "-"}</p>
+							</div>
+							<div>
+								<p><span class="fas fa-calendar"></span> ${obj.Birthday || "-"}</p>
+								<p><span class="fas fa-compass"></span> ${obj.Address || "-"}</p>
+							</div>
+						</div>`
+				}
 			]
 		};
+	}
+
+	$getContactsHeader() {
+		return this.$$("contactsHeader");
 	}
 
 	$getContactsTemplate() {
@@ -57,7 +56,7 @@ export default class ContactsTemplate extends JetView {
 	createLabel(label, title, style) {
 		return {
 			view: "label",
-			label,
+			label: label,
 			name: title,
 			css: style
 		};
@@ -79,10 +78,8 @@ export default class ContactsTemplate extends JetView {
 			statusesData.waitData
 		]).then(() => {
 			if (contactId) {
+				this.$getContactsHeader().parse(contactsData.getItem(contactId));
 				this.$getContactsTemplate().parse(contactsData.getItem(contactId));
-				contactsData.data.each((obj) => {
-					obj.StatusID = statusesData.getItem(obj.StatusID).value;
-				});
 			}
 		});
 	}
